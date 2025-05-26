c119bf9f04cb54736e2259b51b24d80d912ac830
Maury SO <lynuxazura@gmail.com>
2025-05-26 16:23:32 -0600

    Edit

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,41 +4,53 @@ function App() {
 
   const { handleChange, file } = useFiles()
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
-  }
-
   return (
     <div className="flex items-center w-full h-dvh pt-15">
-      <div className=" fixed top-0 w-full h-15 bg-green-700">
-        <h1>Bicho</h1>
+      <div className=" fixed top-0 w-full h-15 bg-green-700 flex justify-between items-center">
+        <div
+          className="flex gap-3 items-center"
+        >
+          <img
+            className="w-15 p-2 rounded-4xl"
+            src="../assets/Verde Bendito (Stickers (circulares)).jpg" />
+          <h1
+            className="font-bold text-2xl text-white"
+          >Fuera Bicho
+          </h1>
+        </div>
+        <div
+          className="mr-5 font-bold text-white text-2xl"
+        >
+          Tesji Pionners
+        </div>
       </div>
       {/* Div */}
       <div className="w-full flex">
-        <div className="bg-blue-400 w-full h-dvh">
+        <div className="w-full h-dvh">
           <div className="bg-black w-full flex justify-center items-center">
             <img
-              className="object-contain h-150 max-h-150 w-auto"
+              className="object-contain h-200 max-h-200 w-auto"
               src={file} alt="" />
           </div>
-          <div className="h-1/2 w-full flex justify-center p-10">
+          <div className="h-auto w-full flex justify-center p-10">
             <form
-              onSubmit={handleSubmit}
               className="flex flex-col gap-4 w-1/2">
               <input
                 onChange={handleChange}
                 className="bg-white p-3 rounded-4xl"
                 type="file" />
               <input
-                className="bg-green-600 font-bold text-white rounded-4xl"
+                className="bg-green-600 font-bold text-white rounded-4xl text-2xl"
                 type="submit" value="Evaluar" />
             </form>
           </div>
         </div>
-        <div className="bg-red-400 w-full h-dvh pt-15 flex justify-center items-center p-10">
-          <div className="bg-white rounded-4xl">
-            <p>Text</p>
-            <p>Text</p>
+        <div className="bg-black w-full h-dvh p-10 pt-20 flex max-w-100">
+          <div className="bg-white rounded-4xl p-10 w-full">
+            <p>Probabilidad: 0.00%</p>
+            <p>Probabilidad: 0.00%</p>
+            <p>Probabilidad: 0.00%</p>
+            <p>Probabilidad: 0.00%</p>
           </div>
         </div>
       </div>
